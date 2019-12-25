50a505277e6f7182e89fca5b3599cfca53a6545f
Artur <arturlee_word@hotmail.com>
2019-12-25 02:47:39 +0100

    Updates

---
+++ precache-manifest.573c50f3c75278e2fab59864cf9a8926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fe5a5b0d392aa15ea8d16f9ddc112d",
+    "url": "/ContactList-React/index.html"
+  },
+  {
+    "revision": "56bd313dd9c01e40358d",
+    "url": "/ContactList-React/static/css/main.24937033.chunk.css"
+  },
+  {
+    "revision": "86d1e0be11e0c81d9e48",
+    "url": "/ContactList-React/static/js/2.3007698d.chunk.js"
+  },
+  {
+    "revision": "4cfd4354159b0a32e54c1aaabcb9d79f",
+    "url": "/ContactList-React/static/js/2.3007698d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56bd313dd9c01e40358d",
+    "url": "/ContactList-React/static/js/main.ecfde051.chunk.js"
+  },
+  {
+    "revision": "56b91ec851b26ef91ddf",
+    "url": "/ContactList-React/static/js/runtime-main.4579909b.js"
+  }
+]);
